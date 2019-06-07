7b7e0b6b67c44d10604797532d3655f6994bdbe5
Libin Babu <libinbabup@hotmail.com>
2019-06-07 21:44:48 +0530

    view fixes

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,8 +2,10 @@ export class AppConstants {
   public static BASE_URL = 'http://127.0.0.1:5000/';
   public static BLOCKCHAIN_BASE_URL = 'http://127.0.0.1:3000/api/';
 
+  public static LOGGEDIN_USER = 'loggedInUser';
   public static USER_TYPE = 'userType';
   public static USER_NAME = 'userName';
+  public static SETTINGS_URL = 'settingsUrl';
 
   public static UNIVERSITY = 'university';
   public static COMPANY = 'company';
--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {AppConstants} from './app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,26 @@ export class CommonService {
   }
 
   private _loggedInUser;
-  private _settingsUrl;
 
   get loggedInUser() {
+    this._loggedInUser = JSON.parse(localStorage.getItem(AppConstants.LOGGEDIN_USER));
     return this._loggedInUser;
   }
 
   set loggedInUser(value) {
     this._loggedInUser = value;
+    localStorage.setItem(AppConstants.LOGGEDIN_USER, JSON.stringify(value));
   }
 
+  private _settingsUrl;
 
   get settingsUrl() {
+    this._settingsUrl = localStorage.getItem(AppConstants.SETTINGS_URL);
     return this._settingsUrl;
   }
 
   set settingsUrl(value) {
     this._settingsUrl = value;
+    localStorage.setItem(AppConstants.SETTINGS_URL, value);
   }
 }
